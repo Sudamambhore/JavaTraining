4b5c9448ab6a356d9fd34c184c81fa0aa728d2ad
Sudamambhore <sudamambhore6717@gmail.com>
2022-05-08 01:52:29 +0530

    Initial comment

---
+++ Capgemini Traninig/Assig1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class Assig1{
+public static void main(String args[]){
+
+   int a;
+   int b;
+   int c;
+   int d;
+   int e;
+   Scanner sc=new Scanner(System.in);
+   a=sc.nextInt();
+   System.out.println(a);
+   b=a/10;
+   c=a%10;
+   System.out.println(b);
+   System.out.println(c);
+   d=b+c;
+   System.out.println(d);
+   
+   }
+ }
---
+++ Capgemini Traninig/Assig7.java
@@ -0,0 +1,14 @@
+      //Exercise 4
+
+import java.util.Scanner;
+class Assig7{
+public static void main(String args[])
+ {
+ long hoursWorked = 40;
+ double payRate = 10.0, taxRate = 0.10;
+ System.out.println("Hours Worked: " + hoursWorked );
+ System.out.println("pay Amount : " + (hoursWorked * payRate) );
+ System.out.println("tax Amount : " + (hoursWorked * payRate * taxRate));
+
+ }
+}
