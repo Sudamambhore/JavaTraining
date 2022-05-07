5095a081a776ad14e61f3b9349f23fe001c1e32b
Sudamambhore <sudamambhore6717@gmail.com>
2022-05-08 01:55:02 +0530

    Initial comment

---
+++ Day 1 & Day 2/Assig3.java
@@ -0,0 +1,13 @@
+        //Exercise 2
+
+import java.util.Scanner;
+class Assig3{
+
+ //public class DoubleJeopardy
+ public static void main (String args[] )
+ {
+ double value = 32;
+ System.out.println("A double: " + value); 
+ }
+
+}
---
+++ Day 1 & Day 2/Assig4.java
@@ -0,0 +1,15 @@
+         //Exercise 3
+
+import java.util.Scanner;
+class Assig4{
+public class CharAssassination
+{
+ public static void main ( String[] args )
+ {
+ char ch = 'A';
+ char ch1 = 'A';
+ System.out.println("A char: " + ch );
+ System.out.println("A char: " + ch1 );
+ }
+}
+}
