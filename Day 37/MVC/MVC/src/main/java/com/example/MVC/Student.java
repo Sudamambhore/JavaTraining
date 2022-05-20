f873f29afbbd466e88a396704c2164ce66001daa
Sudamambhore <sudamambhore6717@gmail.com>
2022-05-20 12:59:48 +0530

    MVC ARCHITECTURE

---
+++ Day 37/MVC/MVC/src/main/java/com/example/MVC/Student.java
@@ -0,0 +1,12 @@
+package com.example.MVC;
+
+import lombok.Data;
+
+@Data
+public class Student {
+	private int stud_id;
+	private String First_Name;
+	private String Last_Name;
+	private String stude_mail;
+
+}
