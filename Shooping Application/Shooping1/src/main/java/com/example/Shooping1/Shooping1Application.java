84a507801b294c8167dde63199e64a12d3b976c6
Sudamambhore <sudamambhore6717@gmail.com>
2022-06-01 11:11:30 +0530

    Shooping code

---
+++ Shooping Application/Shooping1/src/main/java/com/example/Shooping1/Shooping1Application.java
@@ -0,0 +1,12 @@
+package com.example.Shooping1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Shooping1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Shooping1Application.class, args);
+	}
+}
